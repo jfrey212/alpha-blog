78437545c2551e0fb58e837b572dc8144e9b4be4
jfrey212 <eojyerf@gmail.com>
2020-07-12 16:28:01 -0500

    Added show action

--- db/migrate/20200623235716_add_timestamps_to_articles.rb
+++ db/migrate/20200623235716_add_timestamps_to_articles.rb
@@ -1,5 +1,6 @@
 class AddTimestampsToArticles < ActiveRecord::Migration[6.0]
   def change
+    #Three arguments - table, action, type
     add_column :articles, :created_at, :datetime
     add_column :articles, :updated_at, :datetime
   end
