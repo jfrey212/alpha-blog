6510aa874ec056d1fef4d678e5293aa80f391d0b
jfrey212 <eojyerf@gmail.com>
2020-07-13 15:59:43 -0500

    Added create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,16 @@ class ArticlesController < ApplicationController
   def index
     @article = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+    #alternately leave out article_path
+    #redirect @article
+  end
 end
