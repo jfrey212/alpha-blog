438aef99e70a3b721e9cf83f489958b3abcd8a8a
jfrey212 <eojyerf@gmail.com>
2020-07-15 21:51:39 -0500

    Added edit action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create] #hides the other routes for articles except :show
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update] #hides the other routes for articles except :show
 
 end
