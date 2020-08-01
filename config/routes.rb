6f751509fa486677ebd0702ce72f8608b140dcf6
jfrey212 <eojyerf@gmail.com>
2020-07-31 23:03:54 -0500

    Finished CRUD operations, links, DRY

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update] #hides the other routes for articles except :show
-
+  resources :articles
 end
